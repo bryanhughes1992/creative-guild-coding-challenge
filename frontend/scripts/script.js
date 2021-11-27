6474b9ca5a901270d0e05a240bc23fb972fcdd25
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2021-11-27 17:25:35 -0500

    formatArtists function complete

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-  fetch("https://creativeguild.bryanhughes.net/api/artists")
+  fetch("https://creative-backend.bryanhughes.net/api/artists")
     .then(response => response.json())
     .then(data => handleArtists(data));
 });
@@ -21,12 +21,15 @@ function handleArtists(data) {
     profilePic = data[i]["profile_pic"];
   }
 
-  console.log(name, phone, email, bio, profilePic);
+  let app = document.getElementById("app");
+  let appContent = formatArtists(name, phone, email, bio, profilePic);
+  app.append(appContent);
 }
 
 function formatArtists(name, phone, email, bio, profilePic) {
   let artistContainer = document.createElement("div");
-  artistContainer.className = "artist-container";
+  artistContainer.classList.add("artist-container");
+
   let artistName = name;
   let artistPhone = phone;
   let artistEmail = email;
@@ -36,21 +39,42 @@ function formatArtists(name, phone, email, bio, profilePic) {
   let nameElement = document.createElement("h1");
   nameElement.innerText = artistName;
   nameElement.classList.add("artist-name");
+  let nameContainer = document.createElement("div");
+  nameContainer.classList.add("artist-name-container");
+  nameContainer.append(nameElement);
+  artistContainer.append(nameContainer);
 
   let phoneElement = document.createElement("p");
   phoneElement.innerText = artistPhone;
   phoneElement.classList.add("artist-phone");
+  let phoneContainer = document.createElement("div");
+  phoneContainer.classList.add("artist-phone-container");
+  phoneContainer.append(phoneElement);
+  artistContainer.append(phoneContainer);
 
   let emailElement = document.createElement("p");
   emailElement.innerText = artistEmail;
   emailElement.classList.add("artist-email");
+  let emailContainer = document.createElement("div");
+  emailContainer.classList.add("artist-email-container");
+  emailContainer.append(emailElement);
+  artistContainer.append(emailContainer);
 
   let bioElement = document.createElement("p");
   bioElement.innerText = artistBio;
   bioElement.classList.add("artist-bio");
+  let bioContainer = document.createElement("div");
+  bioContainer.classList.add("artist-bio-container");
+  bioContainer.append(bioElement);
+  artistContainer.append(bioContainer);
 
   let picElement = document.createElement("img");
   picElement.src = artistPic;
   picElement.alt = "Artist Profile Picture";
+  let picContainer = document.createElement("div");
+  picContainer.classList.add("artist-pic-container");
+  picContainer.append(picElement);
+  artistContainer.append(picContainer);
 
+  return artistContainer;
 }
